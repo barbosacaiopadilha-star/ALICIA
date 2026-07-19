d4fe11667acd07189ba558de8017e6e0a1039f57
AliCIA Dev <dev@aliviar.local>
2026-07-19 01:05:23 +0000

    domain: integrar Specialty e Education em Professional

--- domain/professional/Professional.ts
+++ domain/professional/Professional.ts
@@ -1,21 +1,35 @@
 import { Identity } from "./Identity";
 import { Registration } from "./Registration";
+import { Specialty } from "./Specialty";
+import { Education } from "./Education";
 
 export interface ProfessionalProps {
   id: string;
   identity: Identity;
   registrations?: Registration[];
+  specialties?: Specialty[];
+  education?: Education[];
 }
 
 export class Professional {
   readonly id: string;
   readonly identity: Identity;
   private readonly _registrations: ReadonlyArray<Registration>;
+  private readonly _specialties: ReadonlyArray<Specialty>;
+  private readonly _education: ReadonlyArray<Education>;
 
-  private constructor(id: string, identity: Identity, registrations: Registration[]) {
+  private constructor(
+    id: string,
+    identity: Identity,
+    registrations: Registration[],
+    specialties: Specialty[],
+    education: Education[]
+  ) {
     this.id = id;
     this.identity = identity;
     this._registrations = Object.freeze(registrations);
+    this._specialties = Object.freeze(specialties);
+    this._education = Object.freeze(education);
   }
 
   static create(props: ProfessionalProps): Professional {
@@ -38,10 +52,21 @@ export class Professional {
       registrations.push(registration);
     }
 
-    return new Professional(id, props.identity, registrations);
+    const specialties = [...(props.specialties ?? [])];
+    const education = [...(props.education ?? [])];
+
+    return new Professional(id, props.identity, registrations, specialties, education);
   }
 
   get registrations(): ReadonlyArray<Registration> {
     return this._registrations;
+  }
+
+  get specialties(): ReadonlyArray<Specialty> {
+    return this._specialties;
+  }
+
+  get education(): ReadonlyArray<Education> {
+    return this._education;
   }
 }
